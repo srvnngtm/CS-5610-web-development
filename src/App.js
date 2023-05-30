8f978aa49e37a2bc250963acc9223e4e0318df00
saran <sarav200898@gmail.com>
2023-05-29 20:54:15 -0400

    lab 3 till routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Labs from "./labs";
+import HelloWorld from "./labs/a3/hellow-world";
+import Tuiter from "./tuiter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <HelloWorld/>
+      <Labs/>
+      <Tuiter/>
     </div>
   );
 }
---
+++ src/labs/a3/index.js
@@ -0,0 +1,13 @@
+import JavaScript from "./java-script";
+
+function Assignment3() {
+    return (
+      <div>
+        <h1>Assignment 3</h1>
+        <JavaScript/>
+      </div>
+    );
+   }
+   export default Assignment3;
+   
+   
