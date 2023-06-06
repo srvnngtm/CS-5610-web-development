c2ca2b80f03d35eb59480f9b1435c741045abc4d
saran <sarav200898@gmail.com>
2023-06-06 10:37:09 -0400

    lab 4 completed

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,15 +1,32 @@
 import Nav from "../nav";
 import Assignment3 from "./a3";
-import {Link} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
+import Assignment4 from "./a4";
 
+
+
+// function Labs() {
+//   return (
+//     <div>
+//      <Nav/>
+//       <Assignment3 />
+//     </div>
+//   );
+// }
 
 function Labs() {
   return (
     <div>
-     <Nav/>
-      <Assignment3 />
+      <Nav/>
+      <Routes>
+        <Route path="a3"
+         element={<Assignment3/>}/>
+        <Route path="a4"
+         element={<Assignment4/>}/>
+      </Routes>
     </div>
   );
-}
+ }
+ 
 
 export default Labs;
